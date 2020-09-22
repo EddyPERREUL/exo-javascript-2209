d1cc0b55303436ab20c495a8c517db8e376fd02e
EddyPERREUL <e.perreul@gmail.com>
2020-09-22 16:53:08 +0200

    correction ex02

--- src/ex02.js
+++ src/ex02.js
@@ -1,12 +1,12 @@
 //Ecrivez un programme qui affiche tous les nombres de 100 à 0 avec un interval de 2 entre chaque nombre.
 
-//compteur de base à 0
+//compteur de base à 100
 let compteur = 100
 
 do {
     //affiche le compteur
     console.log(compteur) 
-    //Ajoute +2 à compteur
+    //Enlève -2 à compteur
     compteur -= 2
-} while (compteur > 0) //tant que compteur est inférieur à 100
+} while (compteur > 0) //tant que compteur est supérieure à 0
 
